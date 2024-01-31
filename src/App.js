c3e6a6ca4c2281634aa2ed559ccdcbd4d0b1c94c
Adriano-Cardoso <goularta97@gmail.com>
2024-01-31 18:39:09 -0300

    correção de import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Banner from './componentes/Banner/Banner';
-import Formulario from './componentes/Formulario';
+import Formulario from './componentes/Formulario/index';
 import Time from './componentes/Time';
 
 function App() {
