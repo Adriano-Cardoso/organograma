ec6e71301f87d4815bd39fde2084c584d12015f4
Adriano-Cardoso <goularta97@gmail.com>
2024-01-31 18:53:05 -0300

    deploy teste correção

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Banner from './componentes/Banner/Banner.js';
-import Formulario from './componentes/Formulario/index.js';
+import Banner from './componentes/Banner/Banner';
+import Formulario from './componentes/Formulario/index';
 import Time from './componentes/Time/index.js';
 
 function App() {
